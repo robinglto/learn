5168d1cfd4c914bb47460192b8f357fd5a951667
robinglto <robinsomedia@pm.me>
2023-11-29 00:00:58 -0400

    Initial commit

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,46 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+  banner: {
+    key: "wip",
+    dismissible: false,
+    text: <p>🚧 Work in progress / Obra en construcción 🚧</p>,
   },
-  chat: {
-    link: 'https://discord.com',
+  sidebar: {
+    defaultMenuCollapseLevel: 0,
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  useNextSeoProps: () => {
+    const { asPath } = useRouter();
+    if (asPath !== "/") {
+      return {
+        titleTemplate: "%s – robin.do",
+      };
+    }
+    return {
+      titleTemplate: "robin.do",
+    };
+  },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Learn robin.do" />
+      <meta property="og:description" content="Learn front-end development." />
+    </>
+  ),
+  logo: <span>Learn</span>,
   footer: {
-    text: 'Nextra Docs Template',
+    text: "robin.do",
   },
-}
+  i18n: [
+    { locale: "en", text: "English" },
+    { locale: "es", text: "Español" },
+  ],
+  project: {
+    link: "https://github.com/robinglto/learn",
+  },
+  docsRepositoryBase: "https://github.com/robinglto/learn",
+};
 
-export default config
+export default config;
